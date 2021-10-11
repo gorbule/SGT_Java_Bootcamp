5d2b790e84abcc6dceabed0e2343159c4888abae
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-10-11 19:33:53 +0300

    LaunchPad class

---
+++ src/lv/alexandragorbule/rocketscience/LaunchPad.java
@@ -0,0 +1,11 @@
+package lv.alexandragorbule.rocketscience;
+
+public class LaunchPad {
+
+    public static void main(String[] args) {
+//Test
+        Rocket rocket1 = new Rocket();
+        System.out.println("There are " + Rocket.countRockets() + " rocket(-s)");
+
+    }
+}
