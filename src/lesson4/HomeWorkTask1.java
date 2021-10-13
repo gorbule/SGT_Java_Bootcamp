93c35b9a03dff817d880de857a0a5dc8ba89355c
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-10-13 21:48:34 +0300

    HW_Task1. 3.0. version

--- src/lesson4/HomeWorkTask1.java
+++ src/lesson4/HomeWorkTask1.java
@@ -4,50 +4,54 @@ public class HomeWorkTask1 {
 
     public static void main(String[] args) {
 
-        String str = "this is not";
+        String str = "999999-99999";
         System.out.println("Input: " + str);
 
-        //Reading in and ignoring any leading whitespace.
-        String noSpaceStr = str.replaceAll("\\s", "");
+        if (str.matches(".*\\d.*")){
 
-        //Checking if result will be positive/negative number
-        String numberOnly = noSpaceStr.replaceAll("[^0-9,+,-]", "");
-        char[] array = new char[numberOnly.length()];
-        for (int k = 0; k < numberOnly.length(); k++) {
-            array[k] = numberOnly.charAt(k);
-        }
-        char sign = ' ';
-        for (int j = 0; j < array.length; j++) {
-            if (array[j] == '+') {
-                sign = '+';
-            } else if (array[j] == '-'){
-                sign = '-';
-            } else {
-                sign = '+';
+            //Reading in and ignoring any leading whitespace.
+            String noSpaceStr = str.replaceAll("\\s", "");
+
+            //Checking if result will be positive/negative number
+            String numberOnly = noSpaceStr.replaceAll("[^0-9,+,-]", "");
+            char[] array = new char[numberOnly.length()];
+            for (int k = 0; k < numberOnly.length(); k++) {
+                array[k] = numberOnly.charAt(k);
             }
-            break;
-        }
-        //Parsing String to Integer
-        String resultStr = noSpaceStr.replaceAll("[^0-9]", "");
-        int resultInt = Integer.parseInt(resultStr);
+            char sign = ' ';
+            for (int j = 0; j < array.length; j++) {
+                if (array[j] == '+') {
+                    sign = '+';
+                } else if (array[j] == '-'){
+                    sign = '-';
+                } else {
+                continue;
+                }
+                break;
+            }
+            //Parsing String to Integer
+            String resultStr = noSpaceStr.replaceAll("[^0-9]", "");
+            //int resultInt = Integer.parseInt(resultStr);
+            Long resultInt = Long.parseLong(resultStr);
 
-        if (sign == '-') {
-            resultInt = resultInt*-1;
-        } else {}
+            if (sign == '-') {
+                resultInt = resultInt*-1;
+            } else {}
 
-        //Mathematical comparing & Result output
-        int minimalNum = Integer.MIN_VALUE;
-        int maximalNum = Integer.MAX_VALUE;
+            //Mathematical comparing & Result output
+            int minimalNum = Integer.MIN_VALUE;
+            int maximalNum = Integer.MAX_VALUE;
 
-        if (resultInt <= minimalNum) {
-            System.out.println("Output is: " + minimalNum);
-        } else if (resultInt >= maximalNum) {
-            System.out.println("Output is: " + maximalNum);
+            if (resultInt <= minimalNum) {
+                System.out.println("Output is: " + minimalNum);
+            } else if (resultInt >= maximalNum) {
+                System.out.println("Output is: " + maximalNum);
+            } else {
+                System.out.println("Output is: " + resultInt);
+            }
         } else {
-            System.out.println("Output is: " + resultInt);
+            System.out.println("Sorry! In your string no one digit!");
         }
-
-
     }
 
     /**
@@ -57,4 +61,5 @@ public class HomeWorkTask1 {
     static void myAtoi(String s) {
 
     }
+
 }
