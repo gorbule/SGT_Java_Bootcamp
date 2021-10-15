4c2e38f138d933dae52d82b2be687d51fe46b892
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-10-15 21:30:05 +0300

    Lesson 7 - Wrapper Classes example

---
+++ src/lesson7/WrapperClasses.java
@@ -0,0 +1,14 @@
+package lesson7;
+
+public class WrapperClasses {
+
+    public static void main(String[] args) {
+
+        int simpleInt = 5;
+        Integer.toString(simpleInt);
+        Integer.reverseBytes(simpleInt);
+
+    }
+
+
+}
