4d8131cd7afdfeff350cdd333ae61bd14a281049
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-10-25 20:45:02 +0300

    Lesson 12

---
+++ test-project/src/test/java/lv/alexandra/testproject/TestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.alexandra.testproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.stereotype.Controller;
+
+@Controller
+class TestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
