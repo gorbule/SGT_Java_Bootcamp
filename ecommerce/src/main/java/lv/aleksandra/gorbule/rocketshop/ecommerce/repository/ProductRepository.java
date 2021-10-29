67f3a1f0a070c0a51fe9bebdd7d43f477e9b185f
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-10-29 21:33:26 +0300

    Lesson 14.

---
+++ ecommerce/src/main/java/lv/aleksandra/gorbule/rocketshop/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,6 @@
+package lv.aleksandra.gorbule.rocketshop.ecommerce.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
