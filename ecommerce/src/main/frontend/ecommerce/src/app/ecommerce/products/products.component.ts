7e0b3e8731fa02dd2b778a5202f35d949f8df3c3
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-11-05 20:14:51 +0200

    Lesson 17.

--- ecommerce/src/main/frontend/ecommerce/src/app/ecommerce/products/products.component.ts
+++ ecommerce/src/main/frontend/ecommerce/src/app/ecommerce/products/products.component.ts
@@ -6,10 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+    productOrders: ProductOrder[] = [];
+    products: Product[] = [];
+    selectedProductOrder: ProductOrder;
+    private shoppingCartOrders: ProductOrders;
+    sub: Subscription;
+    productSelected: boolean = false;
 
-  constructor() { }
+    constructor(private ecommerceService: EcommerceService) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit() {
+        this.productOrders = [];
+        this.loadProducts();
+        this.loadOrders();
+    }
 
+    loadProducts() {
+        this.ecommerceService.getAllProducts()
+            .subscribe(
+                (products: any[]) => {
+                    this.products = products;
+                    this.products.forEach(product => {
+                        this.productOrders.push(new ProductOrder(product, 0));
+                    })
+                },
+                (error: any) => console.log(error)
+            );
+    }
+
+    loadOrders() {
+        this.sub = this.ecommerceService.OrdersChanged.subscribe(() => {
+            this.shoppingCartOrders = this.ecommerceService.ProductOrders;
+        });
+    }
+    addToCart(order: ProductOrder) {
+        this.ecommerceService.SelectedProductOrder = order;
+        this.selectedProductOrder = this.ecommerceService.SelectedProductOrder;
+        this.productSelected = true;
+    }
+
+    removeFromCart(productOrder: ProductOrder) {
+        let index = this.getProductIndex(productOrder.product);
+        if (index > -1) {
+            this.shoppingCartOrders.productOrders.splice(
+                this.getProductIndex(productOrder.product), 1);
+        }
+        this.ecommerceService.ProductOrders = this.shoppingCartOrders;
+        this.shoppingCartOrders = this.ecommerceService.ProductOrders;
+        this.productSelected = false;
+    }
 }
