309fc0d0eff28aeb67aec7481d47225796206e3d
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-11-06 10:06:19 +0200

    Lesson 18. Tutorial project. Group 1

---
+++ tutorial/src/main/java/lv/group1/tutorial/tutorial/repository/TutorialRepository.java
@@ -0,0 +1,11 @@
+package lv.group1.tutorial.tutorial.repository;
+
+import lv.group1.tutorial.tutorial.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+}
