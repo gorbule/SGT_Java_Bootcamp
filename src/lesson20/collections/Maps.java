b286644d27a275f1ee69636f74215717b2b52fbf
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-11-10 20:26:53 +0200

    Lesson 20.

---
+++ src/lesson20/collections/Maps.java
@@ -0,0 +1,12 @@
+package lesson20.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Maps {
+    public static void main(String[] args) {
+
+        Map<String, Integer> students = new HashMap<String, Integer>();
+
+    }
+}
