7a058aaf1d41540ebf18839e315100e3da8fc826
Aleksandra Gorbule <gorbule2@inbox.lv>
2021-11-12 20:12:14 +0200

    Lesson 21.

--- src/lesson20and21/collections/DequeueLesson21.java
+++ src/lesson20and21/collections/DequeueLesson21.java
@@ -1,7 +1,6 @@
 package lesson20and21.collections;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
 public class DequeueLesson21 {
 
@@ -27,6 +26,64 @@ public class DequeueLesson21 {
         myDeque.addFirst("5");
         System.out.println(myDeque);
 
+        //Example of equals() and hasCode() implementation
+        List<CustomObject> myList = new ArrayList<>();
 
+        CustomObject co1 = new CustomObject();
+        co1.id = 001;
+
+        CustomObject co2 = new CustomObject();
+        co2.id = 002;
+
+        myList.add(co1);
+        myList.add(co2);
+
+        Iterator mlIterator = myList.iterator();
+
+        while (mlIterator.hasNext()) {
+            System.out.println(mlIterator.next());
+        }
+
+                //equals? example
+                String pet1 = "parrot";
+                String pet2 = "penguin";
+
+                if (pet1 == pet2) {
+                    //do something
+                }
+                //the same as
+                if (pet1.equals(pet2)) {
+                    //do something
+                }
+
+                /**
+        if (pet1 == "parrot") {
+            System.out.println("I was wrong");
+        }
+                 */
+
+    }
+}
+
+//equals () and hashCode() ---> Generate automatically
+class CustomObject {
+    String customField;
+    int customInt;
+    int id;
+
+    public CustomObject() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CustomObject)) return false;
+        CustomObject that = (CustomObject) o;
+        return customInt == that.customInt && id == that.id && Objects.equals(customField, that.customField);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customField, customInt, id);
     }
 }
