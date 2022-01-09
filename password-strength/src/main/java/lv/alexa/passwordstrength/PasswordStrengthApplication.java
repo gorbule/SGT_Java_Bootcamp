de1d3401cc8e6a2cd25b0f67241af15d76b58d19
Aleksandra Gorbule <gorbule2@inbox.lv>
2022-01-09 20:53:11 +0200

    Password Strenght

---
+++ password-strength/src/main/java/lv/alexa/passwordstrength/PasswordStrengthApplication.java
@@ -0,0 +1,13 @@
+package lv.alexa.passwordstrength;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordStrengthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordStrengthApplication.class, args);
+	}
+
+}
